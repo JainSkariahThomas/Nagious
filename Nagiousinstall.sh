19aa54ea342c5b94177283c371a4cd3eca7cabe9
root <root@ip-172-31-18-251.us-east-2.compute.internal>
2021-07-04 06:02:54 +0000

    first commit

---
+++ Nagiousinstall.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install -y autoconf gcc libc6 make wget unzip apache2 php libapache2-mod-php7.4 libgd-dev
+cd /tmp
+sudo wget -O nagioscore.tar.gz https://github.com/NagiosEnterprises/nagioscore/archive/nagios-4.4.5.tar.gz
+sudo tar xzf nagioscore.tar.gz
+cd /tmp/nagioscore-nagios-4.4.5/
+sudo ./configure --with-httpd-conf=/etc/apache2/sites-enabled
+sudo make all
+sudo make install-groups-users
+sudo usermod -a -G nagios www-data
+sudo make install
+sudo make install-daemoninit
+sudo make install-commandmode
+sudo make install-config
+sudo make install-webconf
+sudo a2enmod rewrite
+sudo a2enmod cgi
+sudo ufw allow Apache
+sudo ufw reload
+sudo htpasswd -cb /usr/local/nagios/etc/htpasswd.users nagiosadmin Sharonsam2
+sudo systemctl restart apache2.service
+sudo systemctl start nagios.service
+sudo apt-get install -y autoconf gcc libc6 libmcrypt-dev make libssl-dev wget bc gawk dc build-essential snmp libnet-snmp-perl gettext
+cd /tmp
+sudo wget --no-check-certificate -O nagios-plugins.tar.gz https://github.com/nagios-plugins/nagios-plugins/archive/release-2.2.1.tar.gz
+sudo tar zxf nagios-plugins.tar.gz
+cd /tmp/nagios-plugins-release-2.2.1/
+sudo ./tools/setup
+sudo ./configure
+sudo make
+sudo make install
+sudo systemctl restart apache2.service
+sudo systemctl status nagios.service
